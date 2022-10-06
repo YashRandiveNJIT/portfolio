3267213c97cc67af0da00600615e030b18bdc31e
meetmefitdev <69871458+meetmefitdev@users.noreply.github.com>
2022-10-06 02:13:20 -0400

    Build

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import BgAnimation from '../components/BackgrooundAnimation/BackgroundAnimation'
 import Hero from '../components/Hero/Hero';
 import Projects from '../components/Projects/Projects';
 import Technologies from '../components/Technologies/Technologies';
+import Experiences from '../components/Experiences/Experiences';
 import Timeline from '../components/TimeLine/TimeLine';
 import { Layout } from '../layout/Layout';
 import { Section } from '../styles/GlobalComponents';
@@ -17,6 +18,7 @@ const Home = () => {
       <Projects />
       <Technologies />
       <Timeline />
+      <Experiences />
       <Acomplishments />
     </Layout>
   );
